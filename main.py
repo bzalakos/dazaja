6e64c1a3a3ed17e1a097df681d0dc2691de53bbe
Benjamin Zalakos <bzalakos@tourism.australia.com>
2018-05-08 08:58:04 +1000

    Push Due To Change Device
    Nothing in particular, just switching devices, have to save progress.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
     the = MyDude(80, 120)
     blocks = [Square(0, 220), Square(160, 156), Square(64, 220)]
     sigma = time.time()
+    pygame.key.get_prevved = pygame.key.get_pressed()
 
     while True: #Terrible idea, or Best Idea?
         for eve in pygame.event.get():
@@ -30,6 +31,7 @@ def main():
         for block in blocks: block.draw(screen)
         pygame.display.flip()
         sigma = sig
+        pygame.key.get_prevved = pygame.key.get_pressed()
 
 class Thing:
     """Object Entity Actor, whatever, it's a thing, it might do stuff"""
@@ -39,7 +41,10 @@ class Thing:
         self.bbox = self.sprite.get_rect()  # #rekt
         # Yes, you do need this. BBOX uses integer coordinates, you keep insisting on floats.
         self.x_value, self.y_value = self.bbox.x, self.bbox.y = x, y
-        self.xme = enum.Enum('H', 'F W')
+        # not sure how much of this stuff will actually be used generically
+        self.tim = 0
+        self.seri = {}  # Something like
+        self.hse = enum.Enum('H', 'F W')
         self.xse = enum.Enum('X', 'L R')
 
     def get_x(self) -> float:
@@ -62,6 +67,16 @@ class Thing:
 
     x, y = property(get_x, set_x), property(get_y, set_y)
 
+    def chain(self, delta):
+        """Proceed through a series of timed steps, maybe"""
+        the = self.seri.get(self.hse, None)
+        if the:
+            if self.tim <= 0:
+                self.tim = the[0]
+                self.hse = the[1]
+            else:
+                self.tim -= delta
+
     def step(self, delta):
         """Do a little bit of thing, delta is time since last step"""
         pass
@@ -78,34 +93,31 @@ class MyDude(Thing):
         self.jpeed = self.speed * 3
         self.jhei = 65
         self.inje = False
-        self.olje = False
         self.jbegin = self.bbox.y
 
     def step(self, delta: float, blocks: list):
         """Do part of a thing
         Keep track of how long it's been since the last part, so you know how much of it to do"""
-        keys = pygame.key.get_pressed() # Take a snapshot of the keyboard
+        # Take a snapshot of the keyboard, and the state of the previous step, for comparison
+        keys, peys = pygame.key.get_pressed(), pygame.key.get_prevved
         desp, jesp = delta * self.speed, delta * self.jpeed
 
         dote = self.bbox.move((0, 1)).collidelistall(blocks)
         ute = self.bbox.move((0, -1)).collidelistall(blocks)
-        if keys[pygame.K_UP]:
-            if dote and not ute:
-                if not self.olje:
-                    self.inje = True
-                    self.olje = True
-                    self.jbegin = self.bbox.bottom
-                else:
-                    self.y = min([blocks[d].top for d in dote]) - self.bbox.h
+        # If the key was pressed just now, and you are also on the ground and there is room to jump
+        if keys[pygame.K_UP] and not peys[pygame.K_UP] and dote and not ute:
+            self.inje = True    # Then begin the jump
+            self.jbegin = self.bbox.bottom
+        if keys[pygame.K_UP]:   # If you're just holding the button down
             if ute or self.bbox.bottom < self.jbegin - self.jhei:
-                self.inje = False
+                self.inje = False   # If you hit something (like your max height), no longer rising
+        else:   # let go early to jump-cancel
+            self.inje = False
+
+        if dote and not self.inje:  # If on the ground and not jumping, be on the ground
+            self.y = min([blocks[d].top for d in dote]) - self.bbox.h
+        else:   # Otherwise, be jumping or falling, whichever the case may be
             self.y -= jesp if self.inje else -jesp
-        else:
-            self.inje, self.olje = False, False
-            if dote:
-                self.y = min([blocks[d].top for d in dote]) - self.bbox.h
-            else:
-                self.y += jesp
 
         if keys[pygame.K_LEFT]:
             lete = self.bbox.move((-1, 0)).collidelistall(blocks)
@@ -125,8 +137,14 @@ class Scissors(Thing):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, sprite=sprites.scissor, **kwargs)
         self.speed = 96
-        self.stae = enum.Enum('stae', '')
-        self.sta =
+        self.hse = enum.Enum('H', 'F W C A R')
+        self.hs = self.hse.F
+        self.xs = self.xse.R
+        self.seri = {'C': (0.5, self.hse.A), 'A': (0.25, self.hse.R), 'R': (0.75, self.hse.F)}
+
+    def step(self, delta: float, blocks: list, guy: Thing):
+        """Loiter, pursue, brace, strike, loiter"""
+        self.chain(delta)
 
 class Square(Thing):
     """Solid Object, one would presume"""
